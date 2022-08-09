e622dfb694593876379dd25f9c5e6042ef20ecdd
madhavijaival <madhavi.jaival113@gmail.com>
2022-08-08 23:25:10 -0700

    ..

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,39 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+function GithubUser({login}){
+const[data, setData] = useState(null);
+useEffect(()=>{
+  fetch(`https://api.github.com/users/${login}`)
+   .then (res =>res.json())
+   .then(setData)
+   .catch(console.error);
+}, []);
 
 
-/* function Lake({name}){
+if(data){
   return(
-  <h1>{name}</h1>
-  );
-} */
-
-const lakeList=["Lake Tahoe", "Lonar", "Salt Lake"];
-
-function App({lakes}){
-  // return(
-  // <div>
-  // <Lake name = "Lake Tahoe" />
-  // <Lake name = "Lonar" />
-  // </div>
-  // );
-  return(
-    <ul>
-      {lakes.map(lake =>(
-        <li>{lake}</li>
-      ))}
-    </ul>
-  );
+    <div>
+      <h1>
+        {data.login}
+      </h1>
+      <img src={data.avatar_url} width ={200}></img>
+    </div>
+  )
+}
 
 }
 
+function App(){
+  return <GithubUser login="madhavijaival"/>;
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App lakes ={lakeList}></App>
-
-
+  <App/>
 );
 
 
